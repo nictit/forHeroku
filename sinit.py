c2702d2f20e66b1e116c2926cb15f78e1e5331d5
nictit <nic392@yandex.ru>
2021-12-03 12:33:25 +0900

    test

--- sinit.py
+++ sinit.py
@@ -139,7 +139,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=["start"])
 def start_message(message):
-    bot.send_message(message.chat.id, 'уже ищу...')
+    bot.send_message(message.chat.id, ' ищу...')
     bot.send_message(message.chat.id, main())
 
 
